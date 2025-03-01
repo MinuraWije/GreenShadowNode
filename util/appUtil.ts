0568558260c8855a1627b0027c5440789fd3e516
MinuraWije <minurawije@gmail.com>
2025-03-01 13:29:38 +0530

    updated util

--- util/appUtil.ts
+++ util/appUtil.ts
@@ -2,7 +2,7 @@ import {UploadedFile} from "express-fileupload";
 import {Request} from "express";
 
 export function extractImg(req: Request) {
-    const image = req.files?.img as UploadedFile
+    const image = req.files?.img  as UploadedFile
     const imageBuffer = image ? image.data : Buffer.alloc(0)
     return imageBuffer.toString("base64")
 }
