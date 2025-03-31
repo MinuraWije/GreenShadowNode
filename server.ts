87d21cf3f7e14dfcbff62a81e0c6e430e20c527e
MinuraWije <minurawije@gmail.com>
2025-03-31 18:33:53 +0530

    updated cors policy

--- server.ts
+++ server.ts
@@ -23,7 +23,7 @@ app.use(fileUpload());
 
 app.use(cors({
     origin: "http://localhost:5173",
-    methods: ['GET', 'POST'],
+    methods: ['GET', 'POST','PUT','DELETE'],
     credentials: true,
 }))
 
